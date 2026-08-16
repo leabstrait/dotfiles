b8f8b879e142526e317b9aa4c2814c4d1fae85f2
Labin Ojha <kontaktlabin@gmail.com>
2026-08-16 21:38:22 +0545

    bulk

--- progs/emacs/config.el
+++ progs/emacs/config.el
@@ -26,7 +26,78 @@
 ;;(setq indicate-empty-lines t)
 (setq indicate-buffer-boundaries '((top . left) (bottom . left) (up . left) (down . left)))
 
-;;    (set-frame-font "NotoSansM Nerd Font Mono-11" nil t) ;
+(set-frame-font "FiraCode Nerd Font-10.5" nil t) ;
+
+
+      (use-package ligature
+:config
+;; Enable the "www" ligature in every possible major mode
+(ligature-set-ligatures 't '("www"))
+;; Enable traditional ligature support in eww-mode, if the
+;; `variable-pitch' face supports it
+(ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+;; Enable all Cascadia and Fira Code ligatures in programming modes
+(ligature-set-ligatures 'prog-mode
+                      '(;; == === ==== => =| =>>=>=|=>==>> ==< =/=//=// =~
+                        ;; =:= =!=
+                        ("=" (rx (+ (or ">" "<" "|" "/" "~" ":" "!" "="))))
+                        ;; ;; ;;;
+                        (";" (rx (+ ";")))
+                        ;; && &&&
+                        ("&" (rx (+ "&")))
+                        ;; !! !!! !. !: !!. != !== !~
+                        ("!" (rx (+ (or "=" "!" "\." ":" "~"))))
+                        ;; ?? ??? ?:  ?=  ?.
+                        ("?" (rx (or ":" "=" "\." (+ "?"))))
+                        ;; %% %%%
+                        ("%" (rx (+ "%")))
+                        ;; |> ||> |||> ||||> |] |} || ||| |-> ||-||
+                        ;; |->>-||-<<-| |- |== ||=||
+                        ;; |==>>==<<==<=>==//==/=!==:===>
+                        ("|" (rx (+ (or ">" "<" "|" "/" ":" "!" "}" "\]"
+                                        "-" "=" ))))
+                        ;; \\ \\\ \/
+                        ("\\" (rx (or "/" (+ "\\"))))
+                        ;; ++ +++ ++++ +>
+                        ("+" (rx (or ">" (+ "+"))))
+                        ;; :: ::: :::: :> :< := :// ::=
+                        (":" (rx (or ">" "<" "=" "//" ":=" (+ ":"))))
+                        ;; // /// //// /\ /* /> /===:===!=//===>>==>==/
+                        ("/" (rx (+ (or ">"  "<" "|" "/" "\\" "\*" ":" "!"
+                                        "="))))
+                        ;; .. ... .... .= .- .? ..= ..<
+                        ("\." (rx (or "=" "-" "\?" "\.=" "\.<" (+ "\."))))
+                        ;; -- --- ---- -~ -> ->> -| -|->-->>->--<<-|
+                        ("-" (rx (+ (or ">" "<" "|" "~" "-"))))
+                        ;; *> */ *)  ** *** ****
+                        ("*" (rx (or ">" "/" ")" (+ "*"))))
+                        ;; www wwww
+                        ("w" (rx (+ "w")))
+                        ;; <> <!-- <|> <: <~ <~> <~~ <+ <* <$ </  <+> <*>
+                        ;; <$> </> <|  <||  <||| <|||| <- <-| <-<<-|-> <->>
+                        ;; <<-> <= <=> <<==<<==>=|=>==/==//=!==:=>
+                        ;; << <<< <<<<
+                        ("<" (rx (+ (or "\+" "\*" "\$" "<" ">" ":" "~"  "!"
+                                        "-"  "/" "|" "="))))
+                        ;; >: >- >>- >--|-> >>-|-> >= >== >>== >=|=:=>>
+                        ;; >> >>> >>>>
+                        (">" (rx (+ (or ">" "<" "|" "/" ":" "=" "-"))))
+                        ;; #: #= #! #( #? #[ #{ #_ #_( ## ### #####
+                        ("#" (rx (or ":" "=" "!" "(" "\?" "\[" "{" "_(" "_"
+                                     (+ "#"))))
+                        ;; ~~ ~~~ ~=  ~-  ~@ ~> ~~>
+                        ("~" (rx (or ">" "=" "-" "@" "~>" (+ "~"))))
+                        ;; __ ___ ____ _|_ __|____|_
+                        ("_" (rx (+ (or "_" "|"))))
+                        ;; Fira code: 0xFF 0x12
+                        ("0" (rx (and "x" (+ (in "A-F" "a-f" "0-9")))))
+                        ;; Fira code:
+                        "Fl"  "Tl"  "fi"  "fj"  "fl"  "ft"
+                        ;; The few not covered by the regexps.
+                        "{|"  "[|"  "]#"  "(*"  "}#"  "$>"  "^="))
+;; Enables ligature checks globally in all buffers. You can also do it
+;; per mode with `ligature-mode'.
+(global-ligature-mode t))
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
@@ -231,6 +302,12 @@
   (define-key global-map [remap execute-extended-command] 'helm-M-x)
   (define-key global-map [remap find-file] 'helm-find-files)
   (define-key global-map [remap switch-to-buffer] #'helm-mini))
+
+(use-package helm
+  :ensure t
+  :config
+  (setq helm-display-function #'helm-display-buffer-in-own-frame
+        helm-commands-using-frame '(helm-M-x helm-find-files helm-mini)))
 
 (with-eval-after-load 'helm
   (define-key helm-map (kbd "<tab>") 'helm-next-line)
--- progs/emacs/custom.el
+++ progs/emacs/custom.el
@@ -11,7 +11,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#1a1e24" :foreground "#eeeeee"))))
  '(company-tooltip-common ((t (:foreground "#00adb5" :weight bold))))
  '(company-tooltip-scrollbar-thumb ((t (:background "#00adb5"))))
  '(company-tooltip-scrollbar-track ((t (:background "#222831"))))
