0bae1a062ead3d8f9819652fd7370dee69f5e7b8
Labin Ojha <kontaktlabin@gmail.com>
2026-08-25 12:44:08 +0545

    wip

--- progs/emacs/custom.el
+++ progs/emacs/custom.el
@@ -5,9 +5,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(company dockerfile-mode expand-region helm indent-bars lsp-ui magit
-	     marginalia multi-vterm no-littering undo-tree vertico
-	     vterm-toggle web-mode yafolding yaml-mode)))
+   '(company dockerfile-mode expand-region helm indent-bars ligature
+             lsp-mode lsp-ui magit marginalia multi-vterm no-littering
+             posframe undo-tree vertico vterm vterm-toggle web-mode
+             yafolding yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,8 +19,6 @@
  '(company-tooltip-scrollbar-track ((t (:background "#222831"))))
  '(company-tooltip-selection ((t (:background "#1b4950" :foreground "#00adb5" :weight bold))))
  '(cursor ((t (:background "#00adb5"))))
- '(flycheck-fringe-info ((t (:foreground "#00adb5"))))
- '(flycheck-fringe-warning ((t (:foreground "#00adb5"))))
  '(fringe ((t (:background "#1a1e24" :foreground "#008389"))))
  '(helm-candidate-number ((t (:foreground "#008389" :weight bold))))
  '(helm-header ((t (:background "#1a1e24" :foreground "#00adb5"))))
@@ -31,24 +30,10 @@
  '(isearch ((t (:background "#00adb5" :foreground "#1a1e24" :weight bold))))
  '(lazy-highlight ((t (:background "#1b4950" :foreground "#00adb5"))))
  '(link ((t (:foreground "#00adb5" :underline t))))
- '(lsp-ui-doc-background ((t (:background "#222831"))))
- '(lsp-ui-peek-header ((t (:background "#222831" :foreground "#00adb5" :weight bold))))
- '(lsp-ui-peek-highlight ((t (:foreground "#00adb5" :weight bold))))
- '(lsp-ui-peek-selection ((t (:background "#1b4950" :foreground "#00adb5"))))
- '(magit-branch-current ((t (:foreground "#00adb5" :box (:line-width 1 :color "#00adb5")))))
- '(magit-branch-local ((t (:foreground "#00adb5" :weight bold))))
- '(magit-diff-hunk-heading ((t (:background "#222831" :foreground "#00adb5"))))
- '(magit-diff-hunk-heading-highlight ((t (:background "#1b4950" :foreground "#00adb5"))))
- '(magit-section-heading ((t (:foreground "#00adb5" :weight bold))))
- '(markdown-header-face-1 ((t (:foreground "#00adb5" :weight bold :height 1.2))))
- '(markdown-header-face-2 ((t (:foreground "#38bdf8" :weight bold))))
  '(minibuffer-prompt ((t (:foreground "#00adb5" :weight bold))))
  '(mode-line ((t (:background "#222831" :foreground "#00adb5" :box (:line-width 1 :color "#00adb5")))))
  '(mode-line-buffer-id ((t (:foreground "#00adb5" :weight bold))))
  '(mode-line-inactive ((t (:background "#1a1e24" :foreground "#8f9ba8" :box (:line-width 1 :color "#2d3748")))))
  '(posframe-border ((t (:background "#00adb5"))))
  '(region ((t (:background "#1b4950" :foreground "#eeeeee"))))
- '(symbol-overlay-default-face ((t (:background "#1b4950" :foreground "#00adb5" :weight bold))))
- '(vertical-border ((t (:foreground "#008389"))))
- '(web-mode-html-attr-name-face ((t (:foreground "#4fc3f7"))))
- '(web-mode-html-tag-face ((t (:foreground "#00adb5" :weight bold)))))
+ '(vertical-border ((t (:foreground "#008389")))))
--- progs/emacs/early-init.el
+++ progs/emacs/early-init.el
@@ -1,4 +1,4 @@
-;; early-init.el --- Early Initialization Optimization -*- lexical-binding: t; -*-
+;; -*- lexical-binding: t; -*-
 
 ;; Make startup faster by reducing the frequency of garbage collection. This will be set back when startup finishes.
 ;; We also increase Read Process Output Max so Emacs can read more data.
@@ -7,16 +7,6 @@
 ;; About 0.02 faster
 (setq gc-cons-threshold (* 1024 1024 128)  ; 128mb
       gc-cons-percentage 1.0) ; Disable the dynamic percentage trigger to ensure GC frequency is fixed.
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; 1. Startup Speed Optimizations                                                ;;
-;; (setq gc-cons-threshold (* 100 1024 1024)                                        ;;
-;;       read-process-output-max (* 1024 1024)) ; 1MB max read size for fast LSP    ;;
-;;                                                                                  ;;
-;; (add-hook 'emacs-startup-hook                                                    ;;
-;;   (lambda ()                                                                     ;;
-;;           (setq gc-cons-threshold (* 2 1024 1024))))                             ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Runtime performance
 ;; Dial the GC threshold back down so that garbage collection happens more frequently but in less time.
@@ -37,41 +27,12 @@
           (lambda ()
             (setq file-name-handler-alist last-file-name-handler-alist)))
 
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; 2. Package Management (Emacs 29+)                                      ;;
-;; (require 'package)                                                        ;;
-;; (setq package-archives '(("gnu"    . "https://elpa.gnu.org/packages/")    ;;
-;;                          ("nongnu" . "https://elpa.nongnu.org/packages/") ;;
-;;                          ("melpa"  . "https://melpa.org/packages/")))     ;;
-;;                                                                           ;;
-;; Fetch the package list automatically if it doesn't exist                  ;;
-;; (unless package-archive-contents                                          ;;
-;;   (package-refresh-contents))                                             ;;
-;;                                                                           ;;
-;; (require 'use-package)                                                    ;;
-;; (setq use-package-always-ensure t)                                        ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-
 ;; Fix white flash on startup
 ;; Don't do it when using daemon or terminal, because it messes up the background color.
-;; (unless (or (daemonp) (not initial-window-system))
-;;   (setq default-frame-alist '(
-;;                               (foreground-color . "white")
-;;                               (background-color . "#181818"))))
-
-;; Set initial frame background to match your dark theme background immediately
-(add-to-list 'default-frame-alist '(background-color . "#1a1e24"))
-(add-to-list 'initial-frame-alist '(background-color . "#1a1e24"))
-
-(set-face-attribute 'mode-line nil :background "DarkSlateGray" :foreground "white")
-(set-face-attribute 'mode-line-inactive nil :background "gray30" :foreground "gray70")
-
+(unless (or (daemonp) (not initial-window-system))
+  (setq default-frame-alist '(
+                              (foreground-color . "white")
+                              (background-color . "#181818"))))
 
 ;; Disable UI elements before UI initialization.
 ;; For faster startup times. It gives 0.05 sec.
@@ -79,116 +40,24 @@
 (setq tool-bar-mode nil)         ; Disable the tool bar
 (push '(vertical-scroll-bars) default-frame-alist) ; Disable the scroll bar
 
-;; Prevent GUI flash by disabling UI elements at the frame level
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars . nil) default-frame-alist) ; Fixed parameter name
-
-;; Disable the modes natively before the graphical window even draws
-(setq menu-bar-mode nil
-      tool-bar-mode nil
-      scroll-bar-mode nil)
-
 ;;; FONTS
 ;; Startup about 0.01 faster
 (set-face-attribute 'default nil
-                    ;; :font "JetBrains Mono" ; Set your favorite type of font or download JetBrains Mono
-                    :font "FiraCode Nerd Font" ; Set your favorite type of font or download FiraCode Nerd Font Mono
-                    :height 120
+                    :font "FiraCode Nerd Font" ; Set your favorite type of font or download JetBrains Mono
+                    :height 110
                     :weight 'medium)
 ;; This sets the default font on all graphical frames created after restarting Emacs.
 ;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
 ;; are not right unless I also add this method of setting the default font.
-(add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font")) ; Set your favorite font
-
+ (add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font")) ; Set your favorite font
 (setq-default line-spacing 0.12)
 
 (prefer-coding-system 'utf-8)
 
-
-;; Ligatures
-        (use-package ligature
-  :config
-  ;; Enable the "www" ligature in every possible major mode
-  (ligature-set-ligatures 't '("www"))
-  ;; Enable traditional ligature support in eww-mode, if the
-  ;; `variable-pitch' face supports it
-  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-  ;; Enable all Cascadia and Fira Code ligatures in programming modes
-  (ligature-set-ligatures 'prog-mode
-                        '(;; == === ==== => =| =>>=>=|=>==>> ==< =/=//=// =~
-                          ;; =:= =!=
-                          ("=" (rx (+ (or ">" "<" "|" "/" "~" ":" "!" "="))))
-                          ;; ;; ;;;
-                          (";" (rx (+ ";")))
-                          ;; && &&&
-                          ("&" (rx (+ "&")))
-                          ;; !! !!! !. !: !!. != !== !~
-                          ("!" (rx (+ (or "=" "!" "\." ":" "~"))))
-                          ;; ?? ??? ?:  ?=  ?.
-                          ("?" (rx (or ":" "=" "\." (+ "?"))))
-                          ;; %% %%%
-                          ("%" (rx (+ "%")))
-                          ;; |> ||> |||> ||||> |] |} || ||| |-> ||-||
-                          ;; |->>-||-<<-| |- |== ||=||
-                          ;; |==>>==<<==<=>==//==/=!==:===>
-                          ("|" (rx (+ (or ">" "<" "|" "/" ":" "!" "}" "\]"
-                                          "-" "=" ))))
-                          ;; \\ \\\ \/
-                          ("\\" (rx (or "/" (+ "\\"))))
-                          ;; ++ +++ ++++ +>
-                          ("+" (rx (or ">" (+ "+"))))
-                          ;; :: ::: :::: :> :< := :// ::=
-                          (":" (rx (or ">" "<" "=" "//" ":=" (+ ":"))))
-                          ;; // /// //// /\ /* /> /===:===!=//===>>==>==/
-                          ("/" (rx (+ (or ">"  "<" "|" "/" "\\" "\*" ":" "!"
-                                          "="))))
-                          ;; .. ... .... .= .- .? ..= ..<
-                          ("\." (rx (or "=" "-" "\?" "\.=" "\.<" (+ "\."))))
-                          ;; -- --- ---- -~ -> ->> -| -|->-->>->--<<-|
-                          ("-" (rx (+ (or ">" "<" "|" "~" "-"))))
-                          ;; *> */ *)  ** *** ****
-                          ("*" (rx (or ">" "/" ")" (+ "*"))))
-                          ;; www wwww
-                          ("w" (rx (+ "w")))
-                          ;; <> <!-- <|> <: <~ <~> <~~ <+ <* <$ </  <+> <*>
-                          ;; <$> </> <|  <||  <||| <|||| <- <-| <-<<-|-> <->>
-                          ;; <<-> <= <=> <<==<<==>=|=>==/==//=!==:=>
-                          ;; << <<< <<<<
-                          ("<" (rx (+ (or "\+" "\*" "\$" "<" ">" ":" "~"  "!"
-                                          "-"  "/" "|" "="))))
-                          ;; >: >- >>- >--|-> >>-|-> >= >== >>== >=|=:=>>
-                          ;; >> >>> >>>>
-                          (">" (rx (+ (or ">" "<" "|" "/" ":" "=" "-"))))
-                          ;; #: #= #! #( #? #[ #{ #_ #_( ## ### #####
-                          ("#" (rx (or ":" "=" "!" "(" "\?" "\[" "{" "_(" "_"
-                                       (+ "#"))))
-                          ;; ~~ ~~~ ~=  ~-  ~@ ~> ~~>
-                          ("~" (rx (or ">" "=" "-" "@" "~>" (+ "~"))))
-                          ;; __ ___ ____ _|_ __|____|_
-                          ("_" (rx (+ (or "_" "|"))))
-                          ;; Fira code: 0xFF 0x12
-                          ("0" (rx (and "x" (+ (in "A-F" "a-f" "0-9")))))
-                          ;; Fira code:
-                          "Fl"  "Tl"  "fi"  "fj"  "fl"  "ft"
-                          ;; The few not covered by the regexps.
-                          "{|"  "[|"  "]#"  "(*"  "}#"  "$>"  "^="))
-  ;; Enables ligature checks globally in all buffers. You can also do it
-  ;; per mode with `ligature-mode'.
-  (global-ligature-mode t))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- ;; 3. Literate Configuration Load                                               ;;
- (let ((literate-config (expand-file-name "config.org" user-emacs-directory)))   ;;
-   (if (file-exists-p literate-config)                                           ;;
-       (org-babel-load-file literate-config)                                     ;;
-     (message "Warning: config.org file not found in %s" user-emacs-directory))) ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; 4. Isolate Custom Variables
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
+;; Tangle init.org to produce init.el
+(let ((init-el (expand-file-name "init.el" user-emacs-directory))
+      (init-org (expand-file-name "init.org" user-emacs-directory)))
+  (when (file-exists-p init-org)
+    (require 'org)
+    (org-babel-tangle-file init-org init-el)
+    (message "Tangling %s to create %s" init-org init-el)))
